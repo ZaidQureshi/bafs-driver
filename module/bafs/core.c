d0b95c55b38b5a61a07c4475f2fe6089dad0304e
Isaac Gelado <igelado@nvidia.com>
2021-02-23 20:53:24 -0800

    Change build system to use CMake. This enables out-of-tree compilation and automatic compilation of the NVIDIA Driver to get the required symbols

--- module/bafs/core.c
+++ module/bafs/core.c
@@ -6,13 +6,13 @@
 #include <linux/pci.h>
 #include <linux/mm.h>
 
-#include <bafs_ctrl.h>
-#include <bafs_group.h>
-#include <bafs_mem.h>
-#include <bafs_core_ioctl.h>
-#include <bafs_ctrl_ioctl.h>
-#include <bafs_util.h>
-#include <bafs_release.h>
+#include <linux/bafs_ctrl.h>
+#include <linux/bafs_group.h>
+#include <linux/bafs_mem.h>
+#include <linux/bafs_core_ioctl.h>
+#include <linux/bafs_ctrl_ioctl.h>
+#include <linux/bafs_util.h>
+#include <linux/bafs_release.h>
 
 
 MODULE_LICENSE("GPL");
