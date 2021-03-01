de110a532d40b503d9c5b164b8ab60bee746440f
Zaid Qureshi <zaidhorse@gmail.com>
2021-03-01 02:28:38 -0500

    added example for dma mapping cpu mem

---
+++ tests/dma_map_cpu_mem.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <bafs.h>
+
+#define PAGE_SIZE 4096
+
+
+int main(int argc, char* argv[] ) {
+    int ret = 0;
+    unsigned size;
+    unsigned loc;
+    void* addr = NULL;
+    const char* ctrl_name;
+    struct bafs_dma_t dma_handle;
+
+    struct bafs_ctrl_t ctrl_handle;
+
+    if (argc < 3) {
+        fprintf(stderr, "Please specify the memory size and controller.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size = strtoul(argv[1], NULL, 0);
+    ctrl_name = argv[2];
+    loc = BAFS_MEM_CPU;
+
+
+
+    ret = posix_memalign(&addr, 4096, size);
+    if (ret) {
+        perror("Unable to allocate cpu memory with posix_memalign");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+    ret = bafs_core_map(&addr, size, loc);
+
+    if (ret) {
+        perror("Error while pinning memory");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully registered and pinned memory\n");
+
+
+    ret = bafs_ctrl_open(ctrl_name, &ctrl_handle);
+    if (ret) {
+        perror("Error while openning ctrl");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully registered and pinned memory\n");
+
+    dma_handle.dma_addrs = malloc(sizeof(void*) * ((size + PAGE_SIZE - 1) / PAGE_SIZE));
+    if (dma_handle.dma_addrs == NULL) {
+        perror("Error allocating dma addresses");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = bafs_ctrl_dma_map_mem(addr, &dma_handle, &ctrl_handle);
+    if (ret) {
+        perror("Error while dma mapping memory");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully dma mapped cpu memory\n");
+
+
+
+    return EXIT_SUCCESS;
+
+
+}
